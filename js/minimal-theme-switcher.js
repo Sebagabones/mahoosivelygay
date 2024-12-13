48045f801fc62f18bf0e82309f9d46452d71d9f4
Seb <bones@ucc.asn.au>
2024-12-13 19:26:40 +0800

    Initial Commit using Picocss

---
+++ js/minimal-theme-switcher.js
@@ -0,0 +1,95 @@
+/*!
+ * Minimal theme switcher
+ *
+ * Pico.css - https://picocss.com
+ * Copyright 2019-2024 - Licensed under MIT
+ */
+const THEME_OWNER = document.documentElement;
+const THEME_STORAGE_KEY = 'theme';
+const cachedTheme = localStorage.getItem(THEME_STORAGE_KEY);
+if (cachedTheme) {
+  THEME_OWNER.dataset[THEME_STORAGE_KEY] = cachedTheme;
+
+  document.querySelector("html")?.setAttribute(this.rootAttribute, cachedTheme);
+
+}
+document.addEventListener('DOMContentLoaded', () => {
+const themeSwitcher = {
+  // Config
+  _scheme: "auto",
+  menuTarget: "details.dropdown",
+  buttonsTarget: "a[data-theme-switcher]",
+  buttonAttribute: "data-theme-switcher",
+  rootAttribute: "data-theme",
+  localStorageKey: "picoPreferredColorScheme",
+
+  // Init
+  init() {
+    this.scheme = this.schemeFromLocalStorage;
+    this.initSwitchers();
+  },
+
+  // Get color scheme from local storage
+  get schemeFromLocalStorage() {
+    return window.localStorage?.getItem(this.localStorageKey) ?? this._scheme;
+  },
+
+  // Preferred color scheme
+  get preferredColorScheme() {
+    return window.matchMedia("(prefers-color-scheme: dark)").matches ? "light" : "dark";
+  },
+
+  // Init switchers
+  initSwitchers() {
+    const buttons = document.querySelectorAll(this.buttonsTarget);
+    buttons.forEach((button) => {
+      button.addEventListener(
+        "click",
+        (event) => {
+          event.preventDefault();
+          // Set scheme
+          this.scheme = button.getAttribute(this.buttonAttribute);
+          // Close dropdown
+          document.querySelector(this.menuTarget)?.removeAttribute("open");
+        },
+        false
+      );
+    });
+  },
+
+  // Set scheme
+  set scheme(scheme) {
+    if (scheme == "auto") {
+      this._scheme = this.preferredColorScheme;
+      delete THEME_OWNER.dataset[THEME_STORAGE_KEY];
+      localStorage.removeItem(THEME_STORAGE_KEY);
+    } else if (scheme == "dark" || scheme == "light") {
+      this._scheme = scheme;
+    }
+    this.applyScheme();
+    this.schemeToLocalStorage();
+    THEME_OWNER.dataset[THEME_STORAGE_KEY] = this.scheme;
+    localStorage.setItem(THEME_STORAGE_KEY, this.scheme);
+
+  },
+
+  // Get scheme
+  get scheme() {
+    return this._scheme;
+  },
+
+  // Apply scheme
+  applyScheme() {
+    document.querySelector("html")?.setAttribute(this.rootAttribute, this.scheme);
+  },
+    // Store scheme to local storage
+  schemeToLocalStorage() {
+    window.localStorage?.setItem(this.localStorageKey, this.scheme);
+  },
+
+};
+
+
+// Init
+themeSwitcher.init();
+});
