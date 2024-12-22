6092831775f84c9c27fe08323cfe655a14bf5c6f
Seb <bones@ucc.asn.au>
2024-12-22 17:21:26 +0800

    test

--- sites/js/anotherThemeChanger.js
+++ sites/js/anotherThemeChanger.js
@@ -1,22 +1,23 @@
-function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
-  if (localStorageTheme !== null) {
-    return localStorageTheme;
-  }
+// function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
+//   if (localStorageTheme !== null) {
+//     return localStorageTheme;
+//   }
 
-  if (systemSettingDark.matches) {
-    return "dark";
-  }
+//   if (systemSettingDark.matches) {
+//     return "dark";
+//   }
 
-  return "light";
-}
+//   return "light";
+// }
 
-const localStorageTheme = localStorage.getItem("theme");
-const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+// const localStorageTheme = localStorage.getItem("theme");
+// const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
 
-let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
-console.log(currentThemeSetting);
-const cachedTheme = localStorage.getItem("theme");
-if (cachedTheme) {
-  document.documentElement.dataset["theme"] = cachedTheme;
-  document.documentElement.querySelector("html")?.setAttribute(this.rootAttribute, cachedTheme);
-}
+// let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
+// console.log(currentThemeSetting);
+// const cachedTheme = localStorage.getItem("theme");
+// if (cachedTheme) {
+//   document.documentElement.dataset["theme"] = cachedTheme;
+//   document.documentElement.querySelector("html")?.setAttribute(this.rootAttribute, cachedTheme);
+// }
+document.documentElement.dataset.theme = localStorage.theme ?? "";
--- sites/js/minimal-theme-switcher.js
+++ sites/js/minimal-theme-switcher.js
@@ -6,65 +6,88 @@ const systemSettingDark2 = window.matchMedia("(prefers-color-scheme: dark)");
 
 // const currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
 document.addEventListener('DOMContentLoaded', () => {
-const themeSwitcher = {
-  // Config
-  _scheme: "auto",
-  menuTarget: "details.dropdown",
-  buttonsTarget: "a[data-theme-switcher]",
-  buttonAttribute: "data-theme-switcher",
-  rootAttribute: "data-theme",
-  localStorageKey: "theme",
+  const themeSwitcher = {
+    // Config
+    _scheme: "auto",
+    menuTarget: "details.dropdown",
+    buttonsTarget: "a[data-theme-switcher]",
+    buttonAttribute: "data-theme-switcher",
+    rootAttribute: "data-theme",
+    localStorageKey: "theme",
 
 
 
 
-  calculateSettingAsThemeString({ localStorageTheme2, systemSettingDark2 }) {
-  if (localStorageTheme2 !== null) {
-    return localStorageTheme2;
-  }
+    // calculateSettingAsThemeString({ localStorageTheme2, systemSettingDark2 }) {
+    //   if (localStorageTheme2 !== null) {
+    //     return localStorageTheme2;
+    //   }
 
-  if (systemSettingDark2.matches) {
-    return "dark";
-  }
+    //   if (systemSettingDark2.matches) {
+    //     return "dark";
+    //   }
 
-  return "light";
-  },
+    //   return "light";
+    // },
 
-  // Init
-  init() {
-    this.scheme = this.calculateSettingAsThemeString({ localStorageTheme2, systemSettingDark2 });
+    // Init
+    init() {
+      // this.scheme = this.calculateSettingAsThemeString({ localStorageTheme2, systemSettingDark2 });
+      this.scheme = localStorage.theme ?? "";
+      console.log(localStorage.theme)
+      this.initSwitchers();
+    },
 
-    this.initSwitchers();
-  },
 
 
+    //   // Init switchers
+    initSwitchers() {
+      const buttons = document.querySelectorAll(this.buttonsTarget);
+      buttons.forEach((button) => {
+        button.addEventListener(
+          "click",
+          (event) => {
+            event.preventDefault();
+            buttonThemePressed = button.getAttribute(this.buttonAttribute);
+            console.log(buttonThemePressed);
+            // if (buttonThemePressed == "dark" || buttonThemePressed == "light") {
 
-//   // Init switchers
-  initSwitchers() {
-    const buttons = document.querySelectorAll(this.buttonsTarget);
-    buttons.forEach((button) => {
-      button.addEventListener(
-        "click",
-        (event) => {
-          event.preventDefault();
-          buttonThemePressed = button.getAttribute(this.buttonAttribute);
+            //   document.querySelector("html")?.setAttribute(this.rootAttribute, buttonThemePressed);
 
-          if (buttonThemePressed == "dark" || buttonThemePressed == "light") {
+            //   window.localStorage?.setItem(THEME_STORAGE_KEY, buttonThemePressed);
 
-            document.querySelector("html")?.setAttribute(this.rootAttribute, buttonThemePressed);
+            // }
+            
+            document.documentElement.dataset.theme = buttonThemePressed;
 
             window.localStorage?.setItem(THEME_STORAGE_KEY, buttonThemePressed);
 
-          }
-          document.querySelector(this.menuTarget)?.removeAttribute("open");
-        },
-        false
-      );
-    });
-  },
+            // else {
 
-};
+            //   // systemSettingDark3 = window.matchMedia("(prefers-color-scheme: dark)");
+            //   window.localStorage?.setItem(THEME_STORAGE_KEY, buttonThemePressed);
+            //   console.log(systemSettingDark3);
+
+            //   if (systemSettingDark3.matches) {
+            //     document.querySelector("html")?.setAttribute(this.rootAttribute, "dark");
 
 
-themeSwitcher.init();
+            //   }
+            //   document.querySelector("html")?.setAttribute(this.rootAttribute, "light");
+
+
+
+
+            // }
+            document.querySelector(this.menuTarget)?.removeAttribute("open");
+          },
+          false
+        );
+      });
+    },
+
+  };
+
+
+  themeSwitcher.init();
 });
