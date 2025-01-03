0e1bcf5c19e5985941021a3f8753ea4bc38ef9cb
Sebastian Gazey <bones@boneswebhome>
2025-01-04 03:51:22 +0800

    songs liveee

---
+++ sites/js/getPlayingNow.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Function to fetch data from the API
+  async function fetchData() {
+    try {
+      // const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const response = await fetch('https://api.mahoosively.gay/getNowPlaying');
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+  // Function to render data in cards
+  async function renderData() {
+    const container = document.querySelector('.playingNow');
+    const data = await fetchData();
+
+    if (!data) {
+      return;
+    }
+
+    item = data;
+    const card = document.createElement('div');
+    card.classList.add('nowPlaying');
+    const paragraph = document.createElement('p');
+    if (item.isPlaying === "true") {
+      paragraph.textContent = `I am currently listening to ${item.songName}, performed by ${item.artist}, and you can see my top songs below:`;
+
+    }
+    else {
+      paragraph.textContent = `Sadly, I am currently not listening to anything - but you can still see my top songs below:`;
+
+    }
+    console.log(paragraph)
+    card.appendChild(paragraph);
+    container.appendChild(card);
+  }
+
+  // Call the renderData function to display data
+  renderData();
+
+  // // document.addEventListener('DOMContentLoaded', () => {
+});
---
+++ sites/js/getTopTracks.js
@@ -0,0 +1,49 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+// Function to fetch data from the API
+async function fetchData() {
+  try {
+    // const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const response = await fetch('https://api.mahoosively.gay/getTopTracks');
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+// Function to render data in cards
+async function renderData() {
+  const container = document.querySelector('.getTopTracks');
+  const data = await fetchData();
+
+  if (!data) {
+    return;
+  }
+
+  data.forEach(item => {
+    const link = document.createElement('a')
+    link.href = item.urlToSong;
+    link.target = "_blank";
+    const card = document.createElement('article');
+    card.classList.add('cardSongs');
+
+    const title = document.createElement('h4');
+    title.textContent = item.songName;
+
+    const body = document.createElement('p');
+    body.textContent = item.artist;
+
+    card.appendChild(title);
+    card.appendChild(body);
+    link.appendChild(card)
+    container.appendChild(link);
+  });
+}
+
+// Call the renderData function to display data
+renderData();
+// // document.addEventListener('DOMContentLoaded', () => {
+});
